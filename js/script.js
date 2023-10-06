090f989a5435c3163514f4ad303837deac89606c
henryv2003 <you@example.com>
2023-10-06 13:11:09 +0200

    ändring öppettid

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const toasts = new Toasts({
 
 /* toasts.push({
     title: 'SPECIELL NOTIS!',
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo sed fugiat maiores alias!',
+    content: 'Thaiköket har stängt lördagen den 7 oktober!',
     style:'success',
 }); */
 
